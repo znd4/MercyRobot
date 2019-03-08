fc0d08fb9373064812411ef9309ab6786db5ea69
Zane Dufour <zane.dufour@gmail.com>
2019-03-08 13:42:07 -0500

    Update OI.java

--- src/main/java/org/usfirst/frc5915/MercyRobot2019/OI.java
+++ src/main/java/org/usfirst/frc5915/MercyRobot2019/OI.java
@@ -82,9 +82,9 @@ public class OI {
         backButton = new JoystickButton(joy, 7);
         startButton.whileHeld(new ReleaseRamp());
         rightTriggerButton = new JoystickButton(joy, 6);
-        rightTriggerButton.whileActive(new LowerArm());
+        rightTriggerButton.whileHeld(new LowerArm());
         leftTriggerButton = new JoystickButton(joy, 5);
-        leftTriggerButton.whileActive(new RaiseArm());
+        leftTriggerButton.whileHeld(new RaiseArm());
 
 
 
